a0deecbca2c67ae75a19cad2e425f88d284ceeb7
Angela <8798027+angelabauer@users.noreply.github.com>
2019-08-20 11:33:15 +0100

    Update sounds from mono to stereo

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
     
+    var player: AVAudioPlayer!
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces sender's (the button that got pressed) opacity to half
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+//        print("Start")
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//            //Code here will be executed after a 2 second delay.
+//            print("End")
+//        }
+        
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
 }
+
+
+
+
+
+
+
 
