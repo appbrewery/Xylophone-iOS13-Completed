e722bd0f6eea907c7d16d4d79f0ee92cf0b5139e
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-09 11:27:12 +0100

    Update code pre-challenge

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -11,23 +11,16 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    var player: AVAudioPlayer!
+    var player = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        
         playSound(soundName: sender.currentTitle!)
-        
-        //Reduces sender's (the button that got pressed) opacity to half
-        sender.alpha = 0.5
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            //Bring's sender's opacity back up to fully opaque.
-            sender.alpha = 1.0
-        }
-        
-//        print("Start")
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-//            //Code here will be executed after a 2 second delay.
-//            print("End")
-//        }
         
     }
     
