05f4ed2c8064af6a60a2ede69c4346d73c78ebdf
angela <angela@londonappbrewery.com>
2019-10-23 12:12:06 +0100

    Update comment

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
         //Reduces the sender's (the button that got pressed) opacity to half.
         sender.alpha = 0.5
         
-        //Code should execute after 2 second delay.
+        //Code should execute after 0.2 second delay.
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
             //Bring's sender's opacity back up to fully opaque.
             sender.alpha = 1.0
